a5202224e3af1977f814abe7955bd952c0db13f1
akwicks1 <akwicks1@gmail.com>
2018-01-05 12:52:01 -0800

    progress on third part

--- minesweeper.js
+++ minesweeper.js
@@ -1,18 +1,38 @@
-const printBoard = (board) => {
-  console.log('Current Board:');
-  console.log(board[0].join(' | '));
-  console.log(board[1].join(' | '));
-  console.log(board[2].join(' | '));
+const generatePlayerBoard = (numberOfRows, numberOfColumns) => {
+  let board = [];
+  for (let i = 0; i < numberOfRows; i++){
+    let row = [];
+    for (let j = 0; j < numberOfColumns; j++){
+      row.push(' ');
+    }
+    board.push(row);
+  }
+  return board;
+
 };
 
-const board = [
-  [' ', ' ', ' '],
-  [' ', ' ', ' '],
-  [' ', ' ', ' '],
-];
+const generateBombBoard = (numberOfRows, numberOfColumns, numberOfBombs) => {
+  let board = [];
+  for (let i = 0; i < numberOfRows; i++){
+    let row = [];
+    for (let j = 0; j < numberOfColumns; j++){
+      row.push(null);
+    }
+    board.push(row);
+  }
+  return board;
+
+  let numberOfBombsPlaced = 0;
+  while (numberOfBombsPlaced < numberOfBombs){
+  let randomRowIndex = Math.floor(Math.random() * numberOfRows);
+  let randomColumnIndex = Math.floor(Math.random() * numberOfColumns);
+  board[randomRowIndex][randomColumnIndex] = 'B';
+  numberOfBombsPlaced++;
+
+    //fix with control flow
+  }};
 
 
-printBoard(board);
-board[0][1] = '1';
-board[2][2] = 'B';
-printBoard(board);
+const printBoard = (board) => {
+  console.log(board.map(row => row.join(' | ')).join('\n'));
+};
